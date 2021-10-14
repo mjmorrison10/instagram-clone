cc919f75208297a437d70fbcad630d3248e6754d
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2021-10-14 14:21:02 -0700

    stories.js

---
+++ data/users.js
@@ -0,0 +1,35 @@
+export const USERS = [
+    {
+        user: 'Alex Frontier',
+        image: 'https://image.shutterstock.com/image-photo/young-man-studio-looking-cameraportrait-260nw-139246634.jpg',
+    },
+    {
+        user: 'Hayley Williams',
+        image: 'https://media.allure.com/photos/5ebeba16caec100008bd324d/1:1/w_2119,h_2119,c_limit/hayley%20williams%20hair%20january%202010.jpg',
+    },
+    {
+        user: 'Rudi Kuchta',
+        image: 'https://www.mantruckandbus.com/fileadmin/media/bilder/02_19/219_05_busbusiness_interviewHeader_1485x1254.jpg',
+    },
+    {
+        user: 'Bart Simpster',
+        image: 'https://media.istockphoto.com/photos/fit-man-standing-outdoors-in-a-city-picture-id1299360127?b=1&k=20&m=1299360127&s=170667a&w=0&h=1Rvw_UPTnj1LkB543cuCjiLM0xbIdjjin0rw6yaL8bc=',
+    },
+    {
+        user: 'Nick Rupstar',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP7nxz1yioVRsom1BjR4cgbKMpZkRx2Cn4cKOEo8-WPatuCYODTIPFSdx9hJLcabT5Sqo&usqp=CAU',
+    },
+    {
+        user: 'Selena Gomez',
+        image: 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/newscms/2020_48/1644203/selena-gomez-saved-by-the-bell-today-square.jpg',
+    },
+    {
+        user: 'Marina Scotzo',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Woman_1.jpg/768px-Woman_1.jpg',
+    },
+    {
+        user: 'Sarah Henkinks',
+        image: 'https://www.imf.org/external/pubs/ft/fandd/2019/03/images/032019/hendriks-600.jpg',
+    },
+
+]
---
+++ screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import Header from "../components/home/Header";
+import Stories from "../components/home/stories";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+        <Header />
+        <Stories />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "black",
+    flex: 1,
+  },
+});
+export default HomeScreen;
